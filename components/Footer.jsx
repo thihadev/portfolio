c706c42cdece8d9f6f93bbc894d8131bf58fb85e
Thihasoe <thihasoe@comquas.com>
2024-06-22 15:36:53 +0630

    footer quote added

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,16 +2,16 @@ import Socials from './Socials'
 
 export default function Footer() {
     return (
-        <footer className='py-2 items-baseline'>
-            <div className="container mx-auto">
-                <div className='flex flex-col items-center justify-between'>
+        <footer className='py-2 items-baseline text-center sm:text-center'>
+            <div className="mx-auto px-1">
+                <div className='flex flex-col items-center sm:text-center justify-between'>
                     {/* <Socials 
                         containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' 
                         iconsStyles='text-primary dark:text-white/70 text-[20px] hover:text-white dark:hover:text-primary transition-all'
                     /> */}
 
-                    <div className='text-muted-foreground'>
-                        Copyright &copy; Thiha Soe. All rights reserved.
+                    <div className="text-muted-foreground">
+                        "it's not a bug, it's an undocumented feature" 
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
       padding: "2rem",
     },
     screens: {
+      xs: '340px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
